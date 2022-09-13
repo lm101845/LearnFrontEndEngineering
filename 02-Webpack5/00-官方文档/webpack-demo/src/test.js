a5f7415f31d1364ec63226050b6bce2acb162aef
lm101845 <1018457683@qq.com>
2022-09-13 13:28:34 +0800

    指南、起步、管理资源看完

---
+++ 02-Webpack5/00-官方文档/webpack-demo/src/test.js
@@ -0,0 +1,11 @@
+/**
+ * @Author liming
+ * @Date 2022/9/13 11:51
+ **/
+const path = require('path');
+
+console.log(__dirname);
+//E:\01-code\15-LearnWebpack5\LearnWebpack5\02-Webpack5\00-官方文档\webpack-demo\src
+
+console.log(path.resolve(__dirname, 'dist'))
+//E:\01-code\15-LearnWebpack5\LearnWebpack5\02-Webpack5\00-官方文档\webpack-demo\src\dist
---
+++ 02-Webpack5/00-官方文档/webpack-demo/webpack.config.js
@@ -0,0 +1,56 @@
+/**
+ * @Author liming
+ * @Date 2022/9/13 11:40
+ **/
+
+const path = require('path');
+//path 模块提供了一些实用工具，用于处理文件和目录的路径
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        filename:'bundle.js',
+        path:path.resolve(__dirname, 'dist'),
+        /**
+         * path.resolve() 方法会将路径或路径片段的序列解析为绝对路径。
+         *
+         * console.log(__dirname);
+         * //E:\01-code\15-LearnWebpack5\LearnWebpack5\02-Webpack5\00-官方文档\webpack-demo\src
+         *
+         * console.log(path.resolve(__dirname, 'dist'))
+         * //E:\01-code\15-LearnWebpack5\LearnWebpack5\02-Webpack5\00-官方文档\webpack-demo\src\dist
+         */
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/i,
+                use:['style-loader', 'css-loader']
+                /**
+                 * 模块 loader 可以链式调用。链中的每个 loader 都将对资源进行转换。链会逆序执行。第一个 loader 将其结果（被转换后的资源）
+                 * 传递给下一个 loader，依此类推。最后，webpack 期望链中的最后的 loader 返回 JavaScript。
+                 *
+                 * 应保证 loader 的先后顺序：'style-loader' 在前，而 'css-loader' 在后。如果不遵守此约定，webpack 可能会抛出错误。
+                 */
+            },
+            //加载 images 图像
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            //加载 fonts 字体
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ]
+    }
+}
